0424eea5538682b74f55aaf5581045736add9cfe
Maksimenko Egor <ohwh33@gmail.com>
2022-05-18 16:25:41 +0300

    реализовать поиск по ISBN и автору

--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -1,14 +1,32 @@
-﻿namespace Store
+﻿using System.Text.RegularExpressions;
+
+namespace Store
 {
     public class Book
     {
         public int BookId { get; }
+        public string Isbn { get; }
+        public string Author { get; }
         public string Title { get; }
-        public Book(int bookId, string title)
+       
+        public Book(int bookId, string isbn, string author, string title)
         {
             BookId = bookId;
             Title = title;
+            Isbn = isbn;
+            Author = author;
         }
 
+        internal static bool IsIsbn(string s)
+        {
+            if(s == null)
+                return false;
+
+            s = s.Replace("-", "")
+                .Replace(" ", "")
+                .ToUpper();
+
+            return Regex.IsMatch(s, @"^ISBN\d{10}(\d{3})?$");
+        }
     }
 }
